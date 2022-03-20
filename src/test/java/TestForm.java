723707731a373c70fdcb2704a308d768bd796bcb
rvaryanica <romavaryanitsa@yandex.ru>
2022-03-20 19:45:03 +0300

    add step

--- src/test/java/TestForm.java
+++ src/test/java/TestForm.java
@@ -10,6 +10,7 @@ import java.io.File;
 
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
 
 public class TestForm {
 
@@ -22,27 +23,35 @@ public class TestForm {
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", true);
         Configuration.browserCapabilities = capabilities;
-        Configuration.remote="https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         Configuration.baseUrl = "https://demoqa.com";
     }
 
     @Test
     void testPracticeForm() {
-        open("/automation-practice-form");
-        $("#firstName").setValue("Boris");
-        $("#lastName").setValue("Britva");
-        $("#userEmail").setValue("borisbritva@gmail.com");
-        $("#userNumber").setValue("8999818907");
-        $(byText("Male")).click();
-        $(byText("Sports")).click();
-        $("#subjectsInput").setValue("Math").pressEnter();
-        $("#react-select-3-input").setValue("Uttar Pradesh").pressEnter();
-        $("#react-select-4-input").setValue("Agra").pressEnter();
-        $x("//textarea[@placeholder='Current Address']").setValue("USSR");
-        $("#dateOfBirthInput").click();
-        $("[class=react-datepicker__month-select]").selectOptionByValue("8");
-        $("[class=react-datepicker__year-select").selectOption("1990");
-        $(".react-datepicker__day--013").click();
+        step("Открываем тестовую форму", () -> {
+            open("/automation-practice-form");
+        });
+        step("Вводим основную информацию", () -> {
+            $("#firstName").setValue("Boris");
+            $("#lastName").setValue("Britva");
+            $("#userEmail").setValue("borisbritva@gmail.com");
+            $("#userNumber").setValue("8999818907");
+            $(byText("Male")).click();
+            $("#dateOfBirthInput").click();
+            $("[class=react-datepicker__month-select]").selectOptionByValue("8");
+            $("[class=react-datepicker__year-select").selectOption("1990");
+            $(".react-datepicker__day--013").click();
+            $("#subjectsInput").setValue("Math").pressEnter();
+            $(byText("Sports")).click();
+        });
+        step("Вводим место проживания", () -> {
+            $("#react-select-3-input").setValue("Uttar Pradesh").pressEnter();
+            $("#react-select-4-input").setValue("Agra").pressEnter();
+            $x("//textarea[@placeholder='Current Address']").setValue("USSR");
+
+        });
+
         //$("#uploadPicture").uploadFile(new File(("src/test/resources/Pictest.png")));
     }
 }
